311bb4e60609c299503b92e17a91543282f6ca38
Josimar <josimarpereiraleite@gmail.com>
2021-09-28 08:26:57 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,52 +1,83 @@
 /*
-	@File 		main.cpp
+	@File 		tm1628.cpp
 	@Author		JOSIMAR PEREIRA LEITE
 	@country	Brazil
 	@Date		07/06/2021
 	
-
     Copyright (C) 2021  JOSIMAR PEREIRA LEITE
-
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 */
 
-#define F_CPU 16000000UL
+#include "tm1628.h"
 
 #include <avr/io.h>
 
-#include "tm1628.h"
-
-TM1628 tm1628;
-
-const char TM1628_SEG[] = {0x00,0x02,0x08,0x0A};
-const unsigned char TM1628_DIGITS[] = {0x7e,0x30,0x6d,0x79,0x33,0x5b,0x5f,0x70,0x7f,0x7b};
-
-int main(void)
+void TM1628::Write(unsigned char data)
 {
-	_delay_ms(100);
+	for (int i=0; i<8; i++)
+	{
+		TM1628_SCK_LOW; 
+		
+		if (data & (1 << i))
+		TM1628_MOSI_HIGH;
+		else
+		TM1628_MOSI_LOW;
+		
+		TM1628_SCK_HIGH;
+	}
+}
 
-	// INIT 
-	tm1628.Init();
+void TM1628::Command(unsigned char data)
+{
+	TM1628_CS_LOW;
+	
+	TM1628::Write(data);
+	
+	TM1628_CS_HIGH;
+}
+
+void TM1628::Data(unsigned char address, unsigned char data)
+{
+	TM1628::Command(0x44);
+	
+	TM1628_CS_LOW;
+	
+	TM1628::Write(0xC0 | address);
+	TM1628::Write(data);
+	
+	TM1628_CS_HIGH;
+}
+
+void TM1628::SetBright(unsigned char bright)
+{
+	bright = (bright % 8);
+	
+	bright = (0x88 | bright);
+	
+	Command( bright );
+}
+
+void TM1628::Init()
+{
+	DDRD |= (1<<TM1628_MOSI);
+	DDRD |= (1<<TM1628_SCK);
+	DDRD |= (1<<TM1628_CS);
+	
+	TM1628_SCK_HIGH;
+	TM1628_CS_HIGH;
 
 	// COMMAND 1: 7 GRIDS
-	tm1628.Command( 0x00 | 0x03 ); 
-	
-	// COMMAND 2: FIXED ADDRESS
-	tm1628.Command( 0x40 | 0x04 );
-	
-	// COMMAND 3: 
-	tm1628.Command( 0xC0 | 0x00 );
-		
-	// COMMAND 4: TM1628_BRIGHTLESS_2 .. 7
-	tm1628.Command( TM1628_BRIGHTLESS_2 );
+	Command( 0x00 | 0x03 );
 
-	tm1628.Data( TM1628_SEG[ 0 ],  TM1628_DIGITS[ 0 ] );
-	tm1628.Data( TM1628_SEG[ 1 ],  TM1628_DIGITS[ 1 ] );
-	tm1628.Data( TM1628_SEG[ 2 ],  TM1628_DIGITS[ 2 ] );
-	tm1628.Data( TM1628_SEG[ 3 ],  TM1628_DIGITS[ 3 ] );
-	
-    while(1);
+	// COMMAND 2: FIXED ADDRESS
+	Command( 0x40 | 0x04 );
+
+	// COMMAND 3:
+	Command( 0xC0 | 0x00 );
+
+	// COMMAND 4: 001 : Pulse width = 2/16
+	Command( TM1628_DISPLAY_ON | TM1628_BRIGHTLESS_7 );
 }
